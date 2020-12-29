aca439ead179aca8cbf1b98f30c6b71d9f2ad0a3
Thaiten <ewaetke10@gmail.com>
2020-12-29 17:58:35 +0100

    No animation when on mobile

--- visualization-eric/word-cloud/src/App.vue
+++ visualization-eric/word-cloud/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <kinesis-container class="container">
-      <kinesis-element class="main-action" type="depth" :strength="5" tag="div">
+      <kinesis-element class="main-action" type="depth" :strength="isMobile ? 0 : 5" tag="div" :style="'top: '+topDistance">
+        <div ref="container">
+          <h1>Nachrichten Analyse</h1>
           <p>Wir haben 200 Artikel vom 21.12. - 27.12.20 von Focus Online, Spiegel und Deutsche Welle analysiert.</p>
           <p>Das meist-genutzte Wort kam 283 mal vor. <span class="bold">Gib' eine Zahl ein und finde ein Wort mit der Häufigkeit</span></p>
-          <input type="number" name="count" id="" v-model.number="count" @keypress.enter="doSearch">
+          <input type="number" name="count" id="" v-model.number="count" @keypress.enter="doSearch" @click="playSound">
           <button @click="doSearch">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-focus-2" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -21,6 +23,7 @@
             <p>Das Wort kommt <span class="bold">{{springNumber}}</span> mal vor.</p>
             <a :href="'https://de.wikipedia.org/wiki/'+activeWord.term" target="_blank">Mehr Informationen hierzu auf Wikipedia</a>
           </div>
+        </div>
         </kinesis-element>
       <svg class="svg-container" viewBox="0 0 1410 1002" fill="none" xmlns="http://www.w3.org/2000/svg">
         <kinesis-element class="element" :strength="randomStrength()" type="translate" tag="g">
@@ -54,7 +57,7 @@
           <circle cx="180.5" cy="636.5" r="110.5" fill="#F0A868" fill-opacity="0.25"/>
         </kinesis-element>
         <kinesis-element class="element" :strength="randomStrength()" type="translate" tag="g">
-          <circle cx="1224" cy="263" r="79" fill="#F0A868" fill-opacity="0.5"/>
+          <circle cx="1224" cy="263" r="79" fill="#F0A868" fill-opacity="0.35"/>
         </kinesis-element>
         <kinesis-element class="element" :strength="randomStrength()" type="translate" tag="g">
           <circle cx="1056.5" cy="891.5" r="93.5" stroke="#F0A868" stroke-opacity="0.33" stroke-width="34"/>
@@ -90,20 +93,45 @@ export default {
     })
   },
   methods: {
+    playSound(){
+      
+    },
     doSearch(){
       this.searched = true;
       this.animation = true;
-      const result = this.list.reduce((previous, current, i, array) => {
-            return i && Math.abs(array[previous].rawFreq - this.count) < Math.abs(current.rawFreq - this.count) ? previous : i;
-        }, -1);
 
-      this.activeWord = this.list[result];
-      console.log(this.list[result].term)
+      const exactMatches = this.list.filter(term => term.rawFreq == this.count);
+
+      if(exactMatches.length > 0){
+        this.activeWord = exactMatches[this.getRandomInt(exactMatches.length)];
+      }else{
+        this.activeWord = this.list[this.list.reduce((previous, current, i, array) => {
+            return i && Math.abs(array[previous].rawFreq - this.count) < Math.abs(current.rawFreq - this.count) ? previous : i;
+        }, -1)];
+      }
+
       window.setTimeout(() => {this.animation = false}, 1000)
     },
     randomStrength(){
-			return Math.random() * 100;
-		}
+      if(this.isMobile){
+        return 0;
+      }else {
+        return Math.random() * 100;
+      }
+    },
+    getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+  },
+  mounted() {
+
+  },
+  computed: {
+    isMobile() {
+      let check = false;
+      (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+      return check;
+    }
   },
   watch: {
     activeWord(newValue) {
@@ -127,6 +155,9 @@ body{
   color: #01110A;
   margin-top: 60px;
 }
+h1{
+  margin-top: 0;
+}
 a{
   color: #ea8b10;
   text-decoration: none;
@@ -134,12 +165,16 @@ a{
   display: block;
   text-align: left;
 }
-.main-action{
+.main-action {
   pointer-events: all;
   z-index: 5;
   position: relative;
-  top: 40%;
-  display: inline-block;
+  height: calc(100% - 4em);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 2em;
 }
 .main-action p{
   text-align: left;
@@ -157,6 +192,8 @@ a{
 }
 svg.svg-container{
   min-width: 100vw;
+  min-height: 100vh;
+  width: auto;
   height: auto;
   position: absolute;
   top: 0;
@@ -225,12 +262,20 @@ button svg{
 
 .wortBox{
   animation: .5s ease appear;
+  height: 165px;
+  margin-top: 16px;
 }
 
 @keyframes appear {
   0% {
-    transform: translateY(100%);
+    height: 0px;
     opacity: 0;
+  }
+}
+
+@media (max-width: 500px) {
+  svg.svg-container{
+  transform: rotate(90deg) scale(1.8);
   }
 }
 </style>
